fc3ea9a5757a46236057371af5d0d407f39733eb
topassky <juan.orozcoco@gmail.com>
2021-10-02 20:59:11 +0200

    Redireccion de perfil solucionada

--- core/user/views/user/views.py
+++ core/user/views/user/views.py
@@ -16,19 +16,6 @@ class RegistroCreateView(CreateView):
     template_name = 'user/registro.html'
     success_url = reverse_lazy('inicio')
 
-    def post(self, request, *args, **kwargs):
-        data = {}
-        try:
-            action = request.POST['action']
-            if action == 'add':
-                form = self.get_form()
-                data = form.save()
-            else:
-                data['error'] = 'No ha ingresado ninguna opción'
-        except Exception as e:
-            data['error'] = str(e)
-        return JsonResponse(data)
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Registro de usuario'
@@ -42,27 +29,12 @@ class UsuarioUpdateView(UpdateView):
     template_name = 'user/Editar.html'
     success_url = reverse_lazy('Perfil')
 
-    @method_decorator(csrf_exempt)
-    @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         self.object = self.get_object()
         return super().dispatch(request, *args, **kwargs)
 
     def get_object(self, queryset=None):
         return self.request.user
-
-    def post(self, request, *args, **kwargs):
-        data = {}
-        try:
-            action = request.POST['action']
-            if action == 'edit':
-                form = self.get_form()
-                data = form.save()
-            else:
-                data['error'] = 'No ha ingresado a ninguna opción'
-        except Exception as e:
-            data['error'] = str(e)
-        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
